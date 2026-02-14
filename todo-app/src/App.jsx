0db3f66aebc78cd88b7230e80cc68ad54f088f64
Mareola-Mabs <bulun1001@gmail.com>
2026-02-14 02:39:37 +0100

    feat: add login page

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
