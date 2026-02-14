5c04feba2c84f726b7e0626daabe36cf7e60a0d7
Mareola-Mabs <bulun1001@gmail.com>
2026-02-14 02:43:45 +0100

    auth: protect dashboard route with authentication guard

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -4,12 +4,25 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import NotFound from "./components/404";
 
+const AuthDashboard = () => {
+
+  if (!isAuthenticated()) {
+    return <Navigate to="/login" />;
+  }
+
+  return <Dashboard />;
+};
+
+
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
+
+      <Route path="/dashboard" element={<AuthDashboard />} />
+
 
       {/* 404 route */}
       <Route path="*" element={<NotFound />} />
